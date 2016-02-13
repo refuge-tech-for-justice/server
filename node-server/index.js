387fe03919987b49b842b340fc98f8558792acc9
mayuri <mayuri@mit.edu>
2016-02-13 02:53:20 -0500

    cleaned up index.js

--- node-server/index.js
+++ node-server/index.js
@@ -18,7 +18,6 @@ map_to_num["server_1"] = '8071';
 map_to_num["server_2"] = '8071';
 map_to_num["server_3"] = '8071';
 
-
 var get_fwd_number = function(request, response) {
   var twiml = new twilio.TwimlResponse();
   var sender = request.body['From'];
@@ -42,24 +41,6 @@ var get_fwd_number = function(request, response) {
   req.end();
 }
 
-
-
-
-var send_msg = function(to, msg) {
-  var client = twilio(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN);
-  client.sendMessage({
-    to: to,
-    from: config.SENDING_NUMBER,
-    body: msg,
-  }, function(err, responseData) {
-    if (err) {
-      console.log('Error sending: ', err);
-    }
-    console.log(responseData.to);
-    console.log(responseData.body);
-  });
-};
-
 var app = express();
 app.use(bodyParser.urlencoded({
   extended: true
@@ -78,9 +59,3 @@ var server = app.listen(80, function() {
   console.log('Refuge listening at http://%s:%s', host, port);
 
 });
-// .on('error', function(err) {
-//   console.error('Cannot serve on port 80 without root privileges:');
-//   console.error('sudo node index.js');
-// });
-
-// module.exports = sms;
