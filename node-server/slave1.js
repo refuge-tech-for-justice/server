62133f2ad99fd889f4521a35cfdac53c9c2b4532
amin10 <manna@mit.edu>
2016-02-13 03:46:27 -0500

    app.listen

--- node-server/slave1.js
+++ node-server/slave1.js
@@ -10,10 +10,10 @@ var http = require('http');
 var cors = require('cors');
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello World\n");
+// });
 
 
 var sms = function(request, response) {
@@ -24,8 +24,9 @@ var app = express();
 app.use(cors);
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8071);
 app.post('/sms', sms);
+
+app.listen(8071);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
