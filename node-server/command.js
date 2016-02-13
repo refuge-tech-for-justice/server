c7c7b6047a57bd713a6691b0ee8e7ccf887cb85d
amin10 <manna@mit.edu>
2016-02-13 06:36:12 -0500

    area codes

--- node-server/command.js
+++ node-server/command.js
@@ -1,6 +1,8 @@
 var firebase = require('firebase');
 var urls = require('./helpers/utils').urls;
 var twilio = require('twilio');
+var _ = require('lodash');
+var get_area=require('./area_codes');
 
 var store_request = function(url, sender, loc){
     var ref = new firebase(url);
@@ -12,21 +14,24 @@ var store_request = function(url, sender, loc){
 }
 
 var command = function(request, response){
+    console.log("IN: ", _.pick(request.body, ['From', 'Body']));
+
     var twiml = new twilio.TwimlResponse();
     var sender = request.body['From'];
     var msg = request.body['Body'];
 
     if(msg.match(/FOOD/i)){
-        store_request(urls.food, sender, "location");
+        store_request(urls.food, sender, get_area(sender));
     }
     if(msg.match(/water/i)){
-        store_request(urls.water, sender, "location");
+        store_request(urls.water, sender, get_area(sender));
     }
     if(msg.match(/shelter/i)){
-        store_request(urls.shelter, sender, "location");
+        store_request(urls.shelter, sender, get_area(sender));
     }
 
     response.send(twiml.message('HEY')); 
 }
 
+// console.log(get_area("+3022218413489"));
 module.exports=command;
