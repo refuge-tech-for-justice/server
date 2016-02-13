3b0f4d2438fe6cf50178ec0496a6b62d2114c4fb
mayuri <mayuri@mit.edu>
2016-02-13 00:43:02 -0500

    hash to coordinate between master and slaves

--- initial_hash.js
+++ initial_hash.js
@@ -1,5 +1,18 @@
+var twilio = require('twilio');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
+
+
 var ConsistentHashing = require('consistent-hashing');
-var cons = new ConsistentHashing(["node1", "node2", "node3"]);
+var cons = new ConsistentHashing(["server_1", "server_2", "server_3"]);
+var map_to_num = {};
+var TWILIO_AUTH_TOKEN = 'dummy_auth'
+map_to_num["server_1"] = '+16313749574';
+map_to_num["server_2"] = '+16179016321';
+map_to_num["server_3"] = 'dummy_number';
 
 var nodes = {};
 var nums = [
@@ -9,15 +22,13 @@ var nums = [
   '1122121'
 ]; //some unique id based on number and id
 
-nums.forEach(function(c) {
-  var node = cons.getNode(c);
-
-  if (nodes[node]) {
-    nodes[node].push(c);
-  } else {
-    nodes[node] = [];
-    nodes[node].push(c);
-  }
-});
-
-console.log(nodes); //distributes and prints nodes
+var get_fwd_number = function(request, response) {
+  var twiml = new twilio.TwimlResponse();
+  var sender = request.body['From'];
+  var msg = request.body['Body'];
+  var key = sender+msg
+  var server = cons.getNode(key);
+  console.log(map_to_num[server])
+  send_msg(map_to_num[server], msg)
+  response.send(twiml.message('HEY'));
+}
